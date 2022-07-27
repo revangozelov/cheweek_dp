68499372bd9bddfe2f7201f13be9bcd53ad4ef44
revangozelov <gozelov.revan@gmail.com>
2022-07-27 23:52:56 +0400

    rr

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+(async function main() {
+  const {
+    app,
+    session,
+    BrowserWindow
+  } = require('electron')
+  const {
+    ElectronChromeExtensions
+  } = require('electron-chrome-extensions')
+  await app.whenReady()
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-// eslint-disable-next-line global-require
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
+  const browserSession = session.fromPartition('persist:custom')
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+  const extensions = new ElectronChromeExtensions({
+    session: browserSession,
+    createTab(details) {
+      // Optionally implemented for chrome.tabs.create support
     },
-  });
+    selectTab(tab, browserWindow) {
+      // Optionally implemented for chrome.tabs.update support
+    },
+    removeTab(tab, browserWindow) {
+      // Optionally implemented for chrome.tabs.remove support
+    },
+    createWindow(details) {
+      // Optionally implemented for chrome.windows.create support
+    }
+  })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  const browserWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      // Same session given to Extensions class
+      session: browserSession,
+      // Recommended options for loading remote content
+      sandbox: true,
+      contextIsolation: true
+    },
+    icon: __dirname + '/test.ico',
+  })
+  console.log(__dirname + '/test.ico');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  // Adds the active tab of the browser
+  extensions.addTab(browserWindow.webContents, browserWindow);
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+  browserWindow.loadURL('https://app.cheweek.com')
+  browserWindow.show();
+}())
