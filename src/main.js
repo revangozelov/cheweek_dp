a0b3903ad267bc6d31648a05f0385591e5b31d0c
revangozelov <gozelov.revan@gmail.com>
2022-08-04 12:28:59 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,20 +12,19 @@ const tab = tabGroup.addTab({
     title: "Cheweek",
     src: urlGl
 });
-tab.on("webview-dom-ready", (tab) => {
-    tab.activate();
-    event_binder(tab);
-});
+tab.activate();
+event_binder(tab);
 tabGroup.on("tab-added", (tab, tabGroups) => {
     tab.activate();
     event_binder(tab);
 });
+
 function event_binder(tab) {
     tab.webview.addEventListener('page-title-updated', () => {
         tab.setTitle(replaceTitle(tab.webview.getTitle()));
     });
     tab.webview.addEventListener('new-window', (e) => {
-        if(e.disposition == 'new-window'){
+        if (e.disposition == 'new-window') {
             window.open(e.url, '_blank', 'autoHideMenuBar=true,width=1100,height=800,nodeIntegration=no');
         } else {
             tabGroup.addTab({
@@ -35,6 +34,7 @@ function event_binder(tab) {
         }
     });
 }
+
 function replaceTitle(dt) {
     return dt.replace('| Cheweek', '');
 }
