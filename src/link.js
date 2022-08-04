ae60105979626f1078cc9a930e840e0576264984
revangozelov <gozelov.revan@gmail.com>
2022-08-04 18:03:12 +0400

    dfg

--- src/link.js
+++ src/link.js
@@ -1,1 +1,3 @@
-const urlGl = "https://app.cheweek.com/"
+const urlGl = "https://cheweek.elcom.az/cheweek1/public/"
+//const urlGl = "https://app.chewekk.com/"
+//const urlGl = "https://cheweek.azsuvenir.local/cheweek1/public/"
